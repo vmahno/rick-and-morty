3c8f0f1370e59bfaf55af49d44a5102ce3b21583
vmahno <born2be.dud3@gmail.com>
2023-05-10 17:36:13 +0300

    configured pom.xml

---
+++ src/main/java/org/ricknmorty/controller/MovieCharacterController.java
@@ -0,0 +1,41 @@
+package org.ricknmorty.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.ricknmorty.dto.CharacterResponseDto;
+import org.ricknmorty.dto.mapper.MovieCharacterMapper;
+import org.ricknmorty.model.MovieCharacter;
+import org.ricknmorty.service.MovieCharacterService;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/movie-characters")
+public class MovieCharacterController {
+    private final MovieCharacterService characterService;
+    private final MovieCharacterMapper mapper;
+
+    public MovieCharacterController(MovieCharacterService characterService,
+                                    MovieCharacterMapper mapper) {
+        this.characterService = characterService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping("/random")
+    @ApiOperation(value = "get random character")
+    public Object getRandom() {
+        MovieCharacter randomCharacter = characterService.getRandomCharacter();
+        return mapper.toResponseDto(randomCharacter);
+    }
+
+    @GetMapping("/by-name")
+    @ApiOperation(value = "get any character by name or it's part")
+    public List<CharacterResponseDto> findAllByName(@RequestParam("name") String namaPart) {
+        return characterService.findAllByNameContains(namaPart).stream()
+                .map(mapper::toResponseDto)
+                .collect(Collectors.toList());
+    }
+}
