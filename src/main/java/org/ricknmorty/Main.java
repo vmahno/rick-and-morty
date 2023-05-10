f2ca1f3ef8b0c229ede8d43628b6760ce4bf8512
vmahno <born2be.dud3@gmail.com>
2023-05-10 10:00:14 +0300

    configured start point

--- src/main/java/org/ricknmorty/Main.java
+++ src/main/java/org/ricknmorty/Main.java
@@ -1,7 +1,13 @@
 package org.ricknmorty;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
